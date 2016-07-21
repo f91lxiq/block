7d5f36776a576a3095ece004d228052b7605a06c
f91lxiq <f91lxiq@hotmail.com>
2016-07-21 23:11:05 +0900

    a
    a

--- js/items.js
+++ js/items.js
@@ -58,3 +58,66 @@ function Strick(itemDirect){
 	};
 	return item;
 }
+
+
+
+Object.setPrototypeOf(Block.prototype, ItemBase.prototype);
+function Block(itemDirect){
+	var item = new ItemBase(itemDirect, ItemType.Block, GetCell(5,2), GetCell(5,2));
+	item.itemDirect = itemDirect;
+	item.CellUp = function(){
+		var cells = new Array(4);
+		cells[0] = GetCell(0, 0);
+		cells[1] = GetCell(0, 1);
+		cells[2] = GetCell(1, 0);
+		cells[3] = GetCell(1, 1);
+		return cells;
+	};
+	item.CellRight = function(){
+		var cells = new Array(4);
+		cells[0] = GetCell(0, 0);
+		cells[1] = GetCell(0, 1);
+		cells[2] = GetCell(1, 0);
+		cells[3] = GetCell(1, 1);
+		return cells;
+	};
+	item.CellDown = function(){
+		var cells = new Array(4);
+		cells[0] = GetCell(0, 0);
+		cells[1] = GetCell(0, 1);
+		cells[2] = GetCell(1, 0);
+		cells[3] = GetCell(1, 1);
+		return cells;
+	};
+	item.CellLeft = function(){
+		var cells = new Array(4);
+		cells[0] = GetCell(0, 0);
+		cells[1] = GetCell(0, 1);
+		cells[2] = GetCell(1, 0);
+		cells[3] = GetCell(1, 1);
+		return cells;
+	};
+	
+	item.SetCells();
+	
+	item.ChangeToNextDirectChild = function(){
+		if (item.itemDirect == DircetType.UP) {
+			item.itemDirect = DircetType.RIGHT;
+		}else
+		if (item.itemDirect == DircetType.RIGHT) {
+			item.itemDirect = DircetType.DOWN;
+		}else
+		if (item.itemDirect == DircetType.DOWN) {
+			item.itemDirect = DircetType.LEFT;
+		}else
+		if (item.itemDirect == DircetType.LEFT) {
+			item.itemDirect = DircetType.UP;
+		}
+		item.SetCells();
+	};
+	item.ChangeDirectChild = function(direct){
+		item.itemDirect = direct;
+		item.SetCells();
+	};
+	return item;
+}
